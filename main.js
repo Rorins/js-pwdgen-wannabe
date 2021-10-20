e66a795e84a6074139ad41f3c3c9a2a0ff49abe3
Aurora Grippaudo <77445911+Rorins@users.noreply.github.com>
2021-10-20 14:23:31 +0200

    Password

---
+++ main.js
@@ -0,0 +1,11 @@
+/*Nome Utente*/
+const nome = prompt("Mi dici il tuo nome?");
+
+/*Cognome Utente*/
+const cognome = prompt("Mi dici il tuo cognome?");
+
+/*Colore preferito Utente*/
+const colore = prompt("Qual'è il tuo colore preferito?");
+
+/*Stampa dati utente*/
+document.getElementById("password").innerHTML = nome + cognome + colore + "21";
